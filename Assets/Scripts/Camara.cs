18fa8c18d37d420da4a7743e5cccf142b7768b8d
jroca7 <70997708+jroca7@users.noreply.github.com>
2021-05-19 16:30:28 -0300

    subida de repositorio

---
+++ Assets/Scripts/Camara.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+
+    public GameObject player;
+    public Vector2 min;
+    public Vector2 max;
+    public float suavizado;
+    Vector2 velocity;
+
+
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, suavizado);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, suavizado);
+
+        transform.position = new Vector3(Mathf.Clamp(posX, min.x, max.x),Mathf.Clamp(posY, min.y, max.y), transform.position.z);
+    }
+
+
+
+}
---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (collision.transform.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
